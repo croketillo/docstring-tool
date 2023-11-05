ba9baaefddace932ab6213cbc0d8468bf69552c6
croketillo <jose_carrizosa@hotmail.com>
2023-11-05 06:59:01 +0100

    Update docstrings

--- docstring_tool/includedstr.py
+++ docstring_tool/includedstr.py
@@ -1,3 +1,30 @@
+""" 
+This file is (or part of) DOCSTRING TOOL v1.0
+Copyright 2023- Croketillo <croketillo@gmail.com> https://github.com/croketillo
+ 
+DESCIPTION:
+DOCSTRING TOOL is a software to include/remove docstrings at the beginning of-
+any python file found in the specified folder.
+
+                        LICENSE -   GNU GPL-3
+
+This software is protected by the GNU General Public License version 3 (GNU GPL-3).
+You are free to use, modify, and redistribute this software in accordance with the 
+terms of the GNU GPL-3. You can find a copy of the license at the following link: 
+https://www.gnu.org/licenses/gpl-3.0.html.
+
+This software is provided as-is, without any warranties, whether express or implied. 
+Under no circumstances shall the authors or copyright holders be liable for any claims, 
+damages, or liabilities arising in connection with the use of this software.
+If you make modifications to this software and redistribute it, you must comply with 
+the terms of the GNU GPL-3, which includes the obligation to provide the source code 
+for your modifications. Additionally, any derived software must also be under the 
+GNU GPL-3.
+
+For more information about the GNU GPL-3 and its terms, please carefully read the full
+license or visit https://www.gnu.org/licenses/gpl-3.0.html
+"""
+
 import os
 import re
 import sys
